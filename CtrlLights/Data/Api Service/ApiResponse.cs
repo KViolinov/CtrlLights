94f13ac644844babccf249338e217f051a332799
Mert1026 <mertelsenev10@gmail.com>
2025-03-17 04:03:20 +0200

    Initial commit of new app.

---
+++ CtrlLights/Data/Api Service/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace CtrlLights.Data.Api_Service
+{
+    public class ApiResponse<T>
+    {
+        public T? TrafficLightsList { get; set; }
+        public T? AirQualityList { get; set; }
+        public T? AdminUsersList { get; set; }
+        public T? EspDevicesList { get; set; }
+        public T? User { get; set; }
+        public bool? Check { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ CtrlLights/Data/Models/TrafficLights.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtrlLights.Data.Models
+{
+    public class TrafficLights
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool FreeLaneStatus { get; set; }
+        public TimeSpan TimeSpan { get; set; }
+        public int Lane { get; set; }
+        public char LightStatus { get; set; }
+        public string Busyness { get; set; }
+    }
+}
